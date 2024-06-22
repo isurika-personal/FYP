3bd459452542aedab8b2bcedd127bbc8f0a519b4
isurika-2001 <cb009957@students.apiit.lk>
2024-06-22 12:35:02 +0530

    app-test-config-10\

--- server/index.js
+++ server/index.js
@@ -17,9 +17,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.options("*", cors());
+
 app.use(
   cors({
-    origin: "https://travel-persona.vercel.app",
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
+
+    credentials: true,
   })
 );
 
