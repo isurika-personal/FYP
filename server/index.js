d24f1da84d0f22cfe1c2585d3e3b5d42cb51dc8d
isurika-2001 <cb009957@students.apiit.lk>
2024-06-22 12:06:13 +0530

    app-test-config-6

--- server/index.js
+++ server/index.js
@@ -1,35 +1,28 @@
 require("dotenv").config();
-const bodyParser = require("body-parser");
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-
-// project imports
 const connectToDatabase = require("./database");
 
-// Routes
+// Import routes
 const user_typeRoutes = require("./src/routes/user_type");
 const userRoutes = require("./src/routes/user");
 const dataRoutes = require("./src/routes/data");
 
 const app = express();
-
-// Allow CORS requests from your frontend URL
-const corsOptions = {
-  origin: "https://travel-persona.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
-
 const PORT = process.env.PORT || 5000;
 
-// Default route
-app.get("/", (req, res) => {
-  res.send("Welcome to the student management system");
-});
+// CORS options
+const corsOptions = {
+  origin: "https://travel-persona.vercel.app", // Replace with your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow the HTTP methods you need
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow the headers you need
+};
 
-// Other middleware and route setups
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -40,6 +33,11 @@ connectToDatabase();
 app.use("/api", user_typeRoutes);
 app.use("/api", userRoutes);
 app.use("/api", dataRoutes);
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("Welcome to the student management system");
+});
 
 // Start the server
 app.listen(PORT, () => {
