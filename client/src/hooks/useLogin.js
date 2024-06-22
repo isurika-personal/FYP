820aaecaeaeab84a72a951639629697d7c668578
isurika-2001 <cb009957@students.apiit.lk>
2024-06-22 11:45:43 +0530

    app-config-17

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -4,41 +4,39 @@ import config from '../config';
 
 export const useLogin = () => {
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
 
   const login = async (values) => {
     setIsLoading(true);
     setError(null);
 
-    try {
-      const response = await fetch(`${config.apiUrl}api/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(values)
-      });
+    const response = await fetch(config.apiUrl + 'api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values)
+    });
+    const json = await response.json();
 
-      const json = await response.json();
-
-      if (!response.ok) {
-        throw new Error(json.error || 'Failed to login');
-      }
-
-      // Save the user to local storage
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+      console.log(json.error);
+    }
+    if (response.ok) {
+      // save the user to local storage
       localStorage.setItem('user', JSON.stringify(json));
 
-      // Update the auth context
+      // update the auth context
       dispatch({ type: 'LOGIN', payload: json });
 
-      // Navigate to the dashboard
-      window.location.href = '/app/home';
-    } catch (error) {
-      setError(error.message);
-      console.error('There has been a problem with your fetch operation:', error);
-    } finally {
+      // update loading state
       setIsLoading(false);
+
+      // navogaite to the dashboard
+      window.location.href = '/app/home';
     }
   };
 
